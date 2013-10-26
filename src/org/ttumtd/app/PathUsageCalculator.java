df91b605379d1987349616707ca45c9a06bee3e0
vaasanthij <Vjagannath@vaasanthi-mac.ariba.com>
2013-10-26 15:15:55 +0530

    commit 2

--- src/org/ttumtd/app/PathUsageCalculator.java
+++ src/org/ttumtd/app/PathUsageCalculator.java
@@ -1,5 +1,28 @@
 package org.ttumtd.app;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class PathUsageCalculator {
+	
+	public PathUsageCalculator ()
+	{
+		
+	}
+	
+	public static Map<Path, Integer> calculateWeightedPath (List<Path> paths,
+			                             long startTimestamp,
+			                             long endTimestamp) 
+    {
+		Map<Path, Integer> weightedPath = new HashMap<Path, Integer>();
+		// for each path query hadoop and get it
+		for (Path path: paths) {
+		    // for now insert and create a dummy weight
+			weightedPath.put(path, (int)(Math.random() % 10));
+		}
+		return weightedPath;
+	}
+			                          
 
 }
